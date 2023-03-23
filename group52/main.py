87c8758421b3ae5cc2ed6bd881b8b61cb87540dc
Caspar Meijer <public.caspar@gmail.com>
2023-03-23 12:19:54 +0100

    packed image

--- group52/main.py
+++ group52/main.py
@@ -6,7 +6,7 @@ import rawpy
 import numpy as np
 
 from group52.retinanet import model
-
+from matplotlib import pyplot as plt
 
 class GenISP(th.nn.Module):
 
@@ -38,29 +38,42 @@ def load_image(path):
     :param path: path to the raw image.
     :return: loaded image.
     """
+    print(path)
     with rawpy.imread(path) as raw:
+        print(raw)
+
         conversion_matrix = raw.rgb_xyz_matrix
-        raw_img = raw.raw_image
-        h, w = raw.shape[0], raw.shape[1]
-        xyz_img = np.ndarray(raw.shape)
+        raw_image = raw.raw_image_visible.astype(np.int32)
+        new_image = np.zeros((int(raw_image.shape[0]/2), int(raw_image.shape[1]/2), 4), dtype=np.int32)
+        new_image[:, :, 0] = raw_image[0::2, 0::2]
+        new_image[:, :, 1] = raw_image[0::2, 1::2]
+        new_image[:, :, 2] = raw_image[1::2, 0::2]
+        new_image[:, :, 3] = raw_image[1::2, 1::2]
+
+        print(new_image.shape)
+        plt.imshow(new_image[:, :, 2])
+        plt.show()
 
         # average green channels form the packed representation
 
-        # apply CST matrix
-        for h_i in range(h):
-            for w_i in range(w):
-                xyz_img[h_i, w_i] = np.matmul(conversion_matrix, raw_img[h_i, w_i])
+        # # apply CST matrix
+        # for h_i in range(h):
+        #     for w_i in range(w):
+        #         print(conversion_matrix.shape, raw_img[h_i, w_i].shape)
+        #         xyz_img[h_i, w_i] = np.matmul(conversion_matrix, raw_img[h_i, w_i])
 
-    return xyz_img
+    return packed
 
 
 def main():
     object_detector = model.resnet50(num_classes=80)
     object_detector.load_state_dict(th.load('../data/coco_resnet_50_map_0_335_state_dict.pt', map_location=th.device('cpu')))
 
-    images_paths = os.listdir('../data/our_sony/')
+    data_dir = '../data/our_sony/'
+    images_paths = os.listdir(data_dir)
     for p in images_paths:
-        image = load_image(p)
+        image = load_image(data_dir + p)
+        break
 
 
 if __name__ == '__main__':
